f32e26809542ee9d0c31cee2d776cddc884f1696
ginger <26461046+gingerchew@users.noreply.github.com>
2024-05-28 15:30:30 -0400

    Adds initial code for `.find`
    on hold due to issues with tsc and intellisense

--- packages/dom/src/index.ts
+++ packages/dom/src/index.ts
@@ -1,5 +1,15 @@
-import type { N } from '@nocht/core';
+// @ts-ignore
+import type { N } from 'nocht';
+import { isString } from '@nocht/shared';
 
 export function dom(instance: N.Nocht) {
     return instance;    
+}
+
+export function find(this: N.Nocht, selector: string|Element) {
+    if (isString(selector)) {
+        this.forEach((el: Element) => {
+            el;
+        })
+    }
 }
