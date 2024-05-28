f9c4fb755b7a2633e5212661872c2bc735d4ee94
ginger <26461046+gingerchew@users.noreply.github.com>
2024-05-28 08:36:33 -0400

    Removed leftover moonrepo stuff

--- packages/core/__tests__/core.test.ts
+++
@@ -1,9 +0,0 @@
-// @vitest-environment jsdom
-import { Nocht, nocht } from '../src/core';
-import { describe, it, expect } from 'vitest';
-
-describe('@nocht/core', () => {
-    it('should return an instance of Nocht', () => {
-        expect(nocht([])).toBeInstanceOf(Nocht)
-    })
-})
