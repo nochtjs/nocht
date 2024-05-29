6f977d3393a905b26d6cd10b7f44c1c50d7eb483
ginger <26461046+gingerchew@users.noreply.github.com>
2024-05-29 07:20:36 -0400

    Fixes tab size

--- packages/shared/vite.config.ts
+++ packages/shared/vite.config.ts
@@ -4,20 +4,20 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  build: {
-    target: 'esnext',
-    minify: 'esbuild',
-    lib: {
-      // @ts-ignore
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: '@nocht/shared',
-      // the proper extensions will be added
-      fileName: 'index',
-      formats: ['es']
-    }
-  },
-  define: {
-    'import.meta.vitest': 'undefined',
-  },
-  plugins: [dts({ rollupTypes: true })]
+	build: {
+		target: 'esnext',
+		minify: 'esbuild',
+		lib: {
+			// @ts-ignore
+			entry: resolve(__dirname, 'src/index.ts'),
+			name: '@nocht/shared',
+			// the proper extensions will be added
+			fileName: 'index',
+			formats: ['es']
+		}
+	},
+	define: {
+		'import.meta.vitest': 'undefined',
+	},
+	plugins: [dts({ rollupTypes: true })]
 })
